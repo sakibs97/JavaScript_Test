4cbd63dceec1698368f07a6839c2a800c74fc212
Sakib Hossain <156642923+sakibs97@users.noreply.github.com>
2025-04-26 19:50:14 +0000

    Add Variables

---
+++ Test_01/Variables.js
@@ -0,0 +1,20 @@
+const accountNumber = 14325
+let email = "sakib@gmail.com"
+var mobileNumber = "1435743"
+city = "Mirpur"
+let state;
+
+
+// accountNumber = 2000
+email = "dadfdsfdf@gmail.com"
+mobileNumber = "0988888"
+city = "Lak"
+
+/*
+
+*/
+
+console.log(accountNumber);
+
+
+console.table([accountNumber,email,mobileNumber,city,state])
