f03de98934e72e9313d844d33f50a515623f057d
Sakib Hossain <156642923+sakibs97@users.noreply.github.com>
2025-05-04 07:00:22 +0000

    Add initial implementation of primitive and non-primitive datatypes

---
+++ Test_01/DataTS.js
@@ -0,0 +1,32 @@
+// Primitive Datatype
+// String, Number, Boolean, Symbole, Undifine, BigInt, Null
+
+let x = "Dark";
+let y= 657346
+let z= false
+let u = 1213344356566765776757n
+let v;
+
+let w = Symbol(y)
+let b = Symbol(y)
+
+// console.log(typeof w); 
+// console.log(typeof b); 
+
+// Non-Primitive Datatype
+// Arrey, Obbject, Function
+
+let xx = [23, "ssss", 22222n, true, 23.345, Symbol(3424), undefined]
+
+let obj = {
+    name: x,
+    age: 20
+}
+
+console.log(obj);
+
+const mainFunction = function(){
+    console.log("Hello");
+}
+
+
