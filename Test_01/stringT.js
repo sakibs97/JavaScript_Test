55cb13f5fb571366328b4d03165b0b736f01e0da
Sakib Hossain <156642923+sakibs97@users.noreply.github.com>
2025-05-05 22:26:52 +0000

    Add string manipulation examples in stringT.js

---
+++ Test_01/stringT.js
@@ -0,0 +1,14 @@
+const nOne = 'Hossain';
+const nString = "Sakib"
+
+const who = `I am ${nString} ${nOne}` 
+
+// console.log(who);
+
+
+
+const city = new String('Rajshahi')
+
+console.log(city.anchor(7));
+
+
